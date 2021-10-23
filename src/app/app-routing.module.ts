e7a011c529cd246e13620d51f53de715b3511164
Rahul Wadhwa <e0489263@u.nus.edu>
2021-10-24 05:15:56 +0800

    amended router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BanalyticsComponent } from './banalytics/banalytics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "banalytics", component: BanalyticsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
