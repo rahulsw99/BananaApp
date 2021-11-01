55f5ebfd05685332a5c63f4729bb44d5413a682f
Rahul Wadhwa <e0489263@u.nus.edu>
2021-11-02 07:11:40 +0800

    added drop down list and indiv user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BanalyticsIndividualComponent } from './banalytics-individual/banalytics-individual.component';
 import { BanalyticsComponent } from './banalytics/banalytics.component';
 
 const routes: Routes = [
-  { path: "banalytics", component: BanalyticsComponent }
+  { path: "banalytics", component: BanalyticsComponent },
+  { path: "banalytics/individual/:name", component: BanalyticsIndividualComponent}
 ];
 
 @NgModule({
